ebbaebed991c68f48efa1fcde7a484bf3a71270d
huzaifa621 <huzaifasheikh2021@gmail.com>
2023-04-26 15:53:00 +0530

    Add: added basic layout of main section

--- frontend/src/Component/MainSection.jsx
+++ frontend/src/Component/MainSection.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import styles from "./mainSection.module.css";
 
 export const MainSection = () => {
-  return (
-    <div>MainSection</div>
-  )
-}
+   return (
+      <div className={styles.flexBox}>
+         <textarea placeholder="Enter your message here"></textarea>
+         <div className={styles.copyDiv}>
+            <button>COPY</button>
+            <textarea placeholder="Output message will be displayed here"></textarea>
+         </div>
+      </div>
+   );
+};
