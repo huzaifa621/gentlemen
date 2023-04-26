bdb84a8219592b6152316643d360f067bdabb950
huzaifa621 <huzaifasheikh2021@gmail.com>
2023-04-26 17:15:10 +0530

    Add: useState use inside main section component

--- frontend/src/Component/MainSection.jsx
+++ frontend/src/Component/MainSection.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import { useState } from "react";
 import styles from "./mainSection.module.css";
 
 export const MainSection = () => {
+   const [message, setMessage] = useState("");
    return (
       <div className={styles.flexBox}>
-         <textarea placeholder="Enter your message here"></textarea>
+         <textarea
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+            placeholder="Enter your message here"
+         ></textarea>
          <div className={styles.copyDiv}>
             <button>COPY</button>
-            <textarea placeholder="Output message will be displayed here"></textarea>
+            <textarea
+               value={message}
+               placeholder="Output message will be displayed here"
+            ></textarea>
          </div>
       </div>
    );
